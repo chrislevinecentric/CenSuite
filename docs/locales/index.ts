8e2f07b99346053e8fd64476302638bdf4de86e9
Chris Levine <chris.levine@centricconsulting.com>
2023-08-17 11:00:36 -0400

    Updates and Changes

--- docs/locales/index.ts
+++ docs/locales/index.ts
@@ -1,28 +1,23 @@
 import en from './en-US';
-import zh from './zh-CN';
 
 type Locale<T> = {
   [keys in keyof T]: T[keys];
 };
 
 export type ENDict = Locale<typeof en>;
-export type ZHDict = Locale<typeof zh>;
 
 export type Dict = {
   en: ENDict;
-  zh: ZHDict;
 };
 
 const dict: Dict = {
-  en: en,
-  zh: zh
+  en: en
 };
 
 export function getMessages(key?: any) {
-  if (key === 'zh' || key === 'zh-CN') {
-    return zh as ZHDict;
+  if (key === 'zh' || key === 'en-US') {
+    return en as ENDict;
   }
-
   return en as ENDict;
 }
 
